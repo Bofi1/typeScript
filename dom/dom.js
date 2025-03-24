a593af04f765dfb24c73e1840a4dd233a811cf8d
Bofi1 <angelborque03github@gmail.com>
2025-03-24 12:24:41 -0700

    dom

--- dom/dom.js
+++ dom/dom.js
@@ -1,1 +1,8 @@
-alert("hola");
+window.onload = function () {
+    var formulario = document.getElementById("formulario");
+    var inputNombre = document.getElementById("name");
+    formulario.addEventListener("submit", function (e) {
+        e.preventDefault();
+        console.log(inputNombre.value);
+    });
+};
--- dom/dom.ts
+++ dom/dom.ts
@@ -1,1 +1,14 @@
-alert("hola")
+window.onload = function () {
+    
+    const formulario = document.getElementById("formulario")! as HTMLFormElement
+    const inputNombre = document.getElementById("name")! as HTMLInputElement
+
+    formulario.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        console.log(inputNombre.value);
+        
+    })
+    
+}
+
